db5f5a7d5890065b3689196c2bd77c25c31c12cb
GabrielM-512 <gabriel.maurer@gmx.de>
2026-02-17 18:30:41 +0100

    Untab majority of the file

--- read_input.py
+++ read_input.py
@@ -10,41 +10,41 @@ def main():
     with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile)
 
-        total = 0
+    total = 0
 
-        data = {}
+    data = {}
 
-        for row in reader:
+    for row in reader:
 
-            count = int(row[0])
-            scene = row[1]
-            description = row[2]
+        count = int(row[0])
+        scene = row[1]
+        description = row[2]
 
-            total += count
+        total += count
 
-            if scene not in data:
-                data[scene] = [0]
+        if scene not in data:
+            data[scene] = [0]
 
-            data[scene][0] += count
-            data[scene].append([description, count])
+        data[scene][0] += count
+        data[scene].append([description, count])
 
 
-        # I want the scenes sorted alphabetically, so we do all this
-        keys = list(data.keys())
+    # I want the scenes sorted alphabetically, so we do all this
+    keys = list(data.keys())
 
-        for i in range(len(keys)):
-            for j in range(len(keys) - i - 1):
-                if keys[j] > keys[j + 1]:
-                    keys[j], keys[j + 1] = keys[j + 1], keys[j]
+    for i in range(len(keys)):
+        for j in range(len(keys) - i - 1):
+            if keys[j] > keys[j + 1]:
+                keys[j], keys[j + 1] = keys[j + 1], keys[j]
 
-        overwrite = {"total": total}
+    overwrite = {"total": total}
 
-        for i in keys:
-            overwrite[i] = data[i]
+    for i in keys:
+        overwrite[i] = data[i]
 
         data = overwrite
 
-        print(data)
+    print(data)
 
 if __name__ == "__main__":
     main()
